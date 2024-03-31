788c5f306ce93a515ccd982962f48941e332c5a9
Guilherme Duccini <guilhermeduccini@gmail.com>
2024-03-30 21:15:45 -0300

    make pull request to fix sinc

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>Guilherme Duccini - Software Engineer</h1>
+      <h1>Guilherme M. B. Duccini - Software Engineer</h1>
     </main>
   );
 }
